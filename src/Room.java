d1b57bd5110e8436ea10bf356621a02550185d4b
RyanHallberg <ryan.hallberg@gmail.com>
2017-12-04 15:49:52 -0600

    Update Room.java
    Added comments

--- src/Room.java
+++ src/Room.java
@@ -1,21 +1,28 @@
 /**
- * 
- * @author Lee
+ * Room Class
+ * The Room class contains fields and methods needed to create a room within the game.
+ * fields:
+ * 	hasEntered: A boolean used to hold the state of whether the player has entered the room or not.
+ * 	item: An Item object in a room that can be picked up by the Player.
+ * 	locked: A boolean used to hold the state of whether or not a room is locked.
+ * 	name: A String that holds the name of the room.
+ * Date: 02 December 2017
+ * OOP Project 2: The Mansion
+ * @author Team 7
  *
  */
 
 public class Room 
 {
 	private String name; // Name of the room.
-	private Item item; // All items in the room that cna be picked up.
+	private Item item; // All items in the room that can be picked up.
 	
 	private boolean locked; 	// Is the room locked or not?
 	private boolean hasEntered; // Has the player been to the room or not?
-	
-// Constructors:
-	
+
 	/**
-	 * 
+	 * The default constructor creates a room object
+	 * with default values.
 	 */
 	public Room()
 	{
@@ -26,8 +33,9 @@ public class Room
 	}
 	
 	/**
-	 * 
-	 * @param name
+	 * This constructor creates a room object, storing the value in the
+	 * name parameter into the name field.
+	 * @param name The value to be stored in the name field.
 	 */
 	public Room(String name)
 	{
@@ -38,9 +46,10 @@ public class Room
 	}
 
 	/**
-	 * 
-	 * @param name
-	 * @param locked
+	 * This constructor creates a room based on it's name and whether
+	 * or not the room is locked.
+	 * @param name The value to be stored in the name field.
+	 * @param locked The value to be stored in the locked field.
 	 */
 	public Room(String name, boolean locked)
 	{
@@ -51,108 +60,71 @@ public class Room
 	}
 	
 	
-// Getters:
-	
-	/**
-	 * 
-	 * @return
-	 */
+	// Getters:
 	public String getName()
 	{
 		return this.name;
 	}
-	
-	/**
-	 * 
-	 * @return
-	 */
+
 	public Item getItem()
 	{
 		return this.item;
 	}
-	
-	/**
-	 * 
-	 * @return
-	 */
+
 	public boolean isLocked()
 	{
 		return this.locked;
 	}
-	
-	/**
-	 * 
-	 * @return
-	 */
+
 	public boolean hasEntered()
 	{
 		return this.hasEntered;
 	}
 
 	
-// Setters:
-	
-	/**
-	 * 
-	 * @param name
-	 */
+	// Setters:
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 	
-	/**
-	 * 
-	 * @param items
-	 */
 	public void setItem(Item item)
 	{
 		this.item = item;
 	}
-	
-	/**
-	 * Set item's name after object creation.
-	 * 
-	 * @param name
-	 */
+
 	public void setItemName(String name)
 	{
 		this.item.setName(name);
 	}
 	
 	/**
-	 * Set item's type after object creation.
-	 * 
-	 * @param type
+	 * The setItemType sets an int value that is used to determine
+	 * which node the player can use that item on.
+	 * @param type The integer value that represents which node
+	 * 			this item can be used on.
 	 */
 	public void setItemType(int type)
 	{
 		this.item.setType(type);
 	}
 	
-	/**
-	 * 
-	 * @param locked
-	 */
+
 	public void setLocked(boolean locked)
 	{
 		this.locked = locked;
 	}
 	
-	/**
-	 * 
-	 * @param hasEntered
-	 */
 	public void setHasEntered(boolean hasEntered)
 	{
 		this.hasEntered = hasEntered;
 	}
 
 	
-// Functions:
+	// Operations:
 	
 	/**
-	 * Lock the current room.
+	 * The lock method locks the current room.
 	 */
 	public void lock()
 	{
@@ -160,7 +132,7 @@ public class Room
 	}
 	
 	/**
-	 * Unlock the room when a key is used on it.
+	 * The unlock method unlocks the room when a key is used on it.
 	 */
 	public void unlock()
 	{
